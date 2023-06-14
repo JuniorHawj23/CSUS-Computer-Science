469181b51aeee89b924da3f29a0efdc46f822f43
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-14 16:12:07 -0700

    Add files via upload

---
+++ CSC60/lab2.c
@@ -0,0 +1,17 @@
+/* ---------- */
+/* Brian Hert */
+/* Lab 2 */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("\nLab 2. \n\n");
+    printf("Hello World.\n");
+    printf("Hi, Brian Hert. \n\n");
+    printf("Be Better Than You Were Yesterday. \n-Lorenzo Snow\n\n");
+
+    return EXIT_SUCCESS;
+}
+/*------------------------*/
