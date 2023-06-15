98eb6fd73ef9f7e4f6b10e00f93c6c3bbeae3eef
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-15 15:20:05 -0700

    Add files via upload

---
+++ CSC15/SailBoatMain.java
@@ -0,0 +1,13 @@
+package Brian;
+
+public class SailBoatMain {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		// SailBoatHelper.drawSailBoat();
+		SailBoatHelper sailHelper = new SailBoatHelper();
+		sailHelper.drawSailBoat();
+		
+	}
+
+}
