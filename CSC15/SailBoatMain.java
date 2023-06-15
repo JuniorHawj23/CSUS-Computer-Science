e4ce5edd82412a8082eb6236c1600bba9f8648c1
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-15 15:21:06 -0700

    Update SailBoatMain.java

--- CSC15/SailBoatMain.java
+++ CSC15/SailBoatMain.java
@@ -6,8 +6,6 @@ public class SailBoatMain {
 	public static void main(String[] args) {
 		// SailBoatHelper.drawSailBoat();
 		SailBoatHelper sailHelper = new SailBoatHelper();
-		sailHelper.drawSailBoat();
-		
+		sailHelper.drawSailBoat();	
 	}
-
 }
