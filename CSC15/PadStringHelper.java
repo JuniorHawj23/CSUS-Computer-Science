27625092144d0dde7488b0abe8ad4f0a85fedb2d
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-16 00:15:44 -0700

    Update PadStringHelper.java

--- CSC15/PadStringHelper.java
+++ CSC15/PadStringHelper.java
@@ -4,40 +4,41 @@ import java.util.Scanner;
 
 public class PadStringHelper {
 
-   public static void runThree() {
-      for(int i=0; i<3; i++) {
-         startStringBuildingProgram();
-      }
-   }
-
-   public static void startStringBuildingProgram() {
-      StringBuffer sb = new StringBuffer();
-      Scanner sc = new Scanner(System.in);
-      // to store result string
-      String word = inputString(sc);
-      //if length given by user is more than word's length then append '-'
-      int length = inputLength(sc);
+    public static void runThree() {
+        for(int i=0; i<3; i++) {
+            startStringBuildingProgram();
+        }
+    }
+   
+    public static void startStringBuildingProgram() {
+        StringBuffer sb = new StringBuffer();
+        Scanner sc = new Scanner(System.in);
+        // to store result string
+        String word = inputString(sc);
+        //if length given by user is more than word's length then append '-'
+        int length = inputLength(sc);
             
-      if (length > word.length()) {
-         for(int i = 0; i < length - word.length(); i++) {
-         sb.append("-");
-         }
-         sb.append(word);
-      } else {
-      sb.append(word);
-      }
-      System.out.println("The resulting string is " + sb.toString());
-   }
+        if (length > word.length()) {
+            for(int i = 0; i < length - word.length(); i++) {
+            sb.append("-");
+            }
+            sb.append(word);
+        } else {
+        sb.append(word);
+        }
+        System.out.println("The resulting string is " + sb.toString());
+        System.out.println();
+    }
 
-   public static String inputString(Scanner sc) {
-      // user input for a string
-      System.out.print("Please enter a string ");
-      return sc.nextLine();
-   }
+    public static String inputString(Scanner sc) {
+        // user input for a string
+        System.out.print("Please enter a string ");
+        return sc.nextLine();
+    }
 
-   public static int inputLength(Scanner sc) {
-      //user input for a length
-      System.out.print("Please enter the length of the result string ");
-      return sc.nextInt();
-   }   
+    public static int inputLength(Scanner sc) {
+        //user input for a length
+        System.out.print("Please enter the length of the result string ");
+        return sc.nextInt();
+    }   
 }
