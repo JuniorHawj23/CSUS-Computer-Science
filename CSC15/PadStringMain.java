99f22963472e8738f5d4713326c96fb77dc89869
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-16 00:00:50 -0700

    Update PadStringMain.java

--- CSC15/PadStringMain.java
+++ CSC15/PadStringMain.java
@@ -1,5 +1,8 @@
-class PadStringMain {
-    public static void main(String[] arg) {
+package Brian;
+	
+class PadStringMain {	
+    @SuppressWarnings("static-access")
+	public static void main(String[] arg) {
         PadStringHelper helper = new PadStringHelper();
         helper.runThree();
     }
