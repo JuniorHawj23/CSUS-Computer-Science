2b2b7864ca6fdbeadc09c70285279bc0ab766db0
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-16 19:53:09 -0700

    Add files via upload

---
+++ CSC130/Project/CSC130FinalProject/src/Main/helper.java
@@ -0,0 +1,31 @@
+package Main;
+
+import java.awt.Toolkit;
+
+public class helper {
+	public final static int SCREENX= Toolkit.getDefaultToolkit().getScreenSize().width;
+	public final static int SCREENY= Toolkit.getDefaultToolkit().getScreenSize().height;
+	
+	final public static int speed = 80;
+	
+	final public static int walltopHeight = -80;
+	final public static int walltopWidth = 1920;
+	
+	final public static int wallbottomHeight = 120; 
+	final public static int wallbottomWidth = 1920;
+	
+	final public static int wallleftHeight = 1080;
+	final public static int wallleftWidth = 30;
+	
+	final public static int wallrightHeight = 1080;
+	final public static int wallrightWidth = 80;
+	
+	final public static int playerWidth = 120;
+	final public static int playerHeight = 230;
+	
+	final public static int houseHeight = -70;
+	final public static int houseWidth = 300;
+	
+	final public static int starHeight = -70;
+	final public static int starWidth = 90;
+}
