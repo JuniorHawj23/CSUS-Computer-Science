e8c92e8a7b0c88ebe3c3608c53841cc4592d3584
Brian Hert <111545268+JuniorHawj23@users.noreply.github.com>
2023-06-21 01:06:52 -0700

    Update SailBoatMain.java

--- CSC15/SailBoatMain.java
+++ CSC15/SailBoatMain.java
@@ -2,7 +2,6 @@ package Brian;
 
 public class SailBoatMain {
 
-	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		// SailBoatHelper.drawSailBoat();
 		SailBoatHelper sailHelper = new SailBoatHelper();
